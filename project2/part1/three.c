99743de202dd9123720a9981b27c07c0284fdfd3
hirochri <hirochri@umich.edu>
2017-02-06 00:37:03 -0500

    improved C version?

--- project2/part1/three.c
+++ project2/part1/three.c
@@ -17,20 +17,31 @@ int main() {
   //MD5(string, strlen(string), result);
   //MD5 is 32 hex digits We need first 7 to be 27 20 4F 52 20 31 23
 
-  unsigned char answer[7] = {0x27, 0x20, 0x4F, 0x52, 0x20, 0x31, 0x23, 0};
+  //OR doesn't have to be all caps
+  //                          '   space   O     R   space   1     #
+  //unsigned char answer[7] = {0x27, 0x20, 0x4F, 0x52, 0x20, 0x31, 0x23, 0};
+  //unsigned char answer1[7] = {0x27, 0x20, 0x6F, 0x72, 0x20, 0x31, 0x23, 0};
+
+  //Don't need 1st space actually
+
+  unsigned char answer[6] = {0x27, 0x4F, 0x52, 0x20, 0x31, 0x23, 0};
+  unsigned char answer1[6] = {0x27, 0x6F, 0x72, 0x20, 0x31, 0x23, 0};
  
   while(1){
     sprintf(strll, "%lld", num);
     MD5(strll, strlen(strll), result);
 
     counter = 0;
-    for(int i = 0; i < 7; i++){
-      if(result[i] == answer[i]){
+    for(int i = 0; i < 6; i++){
+      if(result[i] == answer[i] || result[i] == answer1[i]){
         counter++;
+      }
+      else{
+        break;
       }
     }
 
-    if(counter == 7){
+    if(counter == 6){
       printf("%llu\n", num);
       for(int i = 0; i < MD5_DIGEST_LENGTH; i++)
         printf("%02x", result[i]);//
