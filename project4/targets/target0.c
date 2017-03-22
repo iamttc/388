452c81003ce15b2b1d427b865ca89b6f6621d555
hirochri <hirochri@umich.edu>
2017-03-22 13:58:08 -0400

    Basic buffer overflow

--- project4/targets/target0.c
+++ project4/targets/target0.c
@@ -5,11 +5,11 @@
 int _main(int argc, char *argv[])
 {
 	char grade[5];
-	char name[10];
+	char name[10]; //Buffer holds 10 bytes
 
 	strcpy(grade, "nil");
 
-	gets(name);
+	gets(name); //We can put in more than 10 bytes..
 
 	printf("Hi %s! Your grade is %s.\n", name, grade);
 	
