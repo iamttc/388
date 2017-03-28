37b3b90f0a213c4299dbfcecc903f8a0be1c6b7b
hirochri <hirochri@umich.edu>
2017-03-27 23:09:34 -0400

    organizeeersed

--- project4/fuzzer.py
+++ project4/fuzzer.py
@@ -21,58 +21,29 @@ def dec_depth():
     depth -= 1
 #####################
 
+#####Random helper shit#########
 def get_int():
     return random.randint(0,10)
+
 def get_chars():
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(get_int()))
 
-# get a tuple of shit
+def get_true():
+    return random.choice(['true', 'True', 'TRUE', 't', 'T', '1'])
+
+def get_false():
+    return random.choice(['false', 'False', 'FALSE', 'f', 'F', '0'])
+
+def get_null():
+    return random.choice(['null', 'Null', 'NULL'])
+
 def get_tuple():
     return '\"' + get_chars() + '\":' + get_value()
+########################
 
 
-def get_number():
-    nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', 'e', 'E', '+', '-']
-    return ''.join([random.choice(nums) for _ in range(0, random.randint(0, 10))])
-
-
-def get_string():
-    fukd = ['\\','\"','\\\\','\/','\b','\f','\r','\t','\u','\u12fd','\uzxdg3'] # ,'\n'
-    temp = '\"'
-    temp += ''.join([random.choice(fukd) for _ in range(0, random.randint(0, 10))])
-    temp += get_chars()
-    temp += '\"'
-    return temp
-
-
-# get json array
-def get_list():
-    if depth_check() == 'FUCK':
-        return ''
-    inc_depth()
-
-    list = '['
-    for i in range(0,4):
-        rand = random.randint(0,4)
-        if rand == 0:
-            list += get_number()
-        elif rand == 1:
-            list += get_string()
-        elif rand == 2:
-            list += get_obj()
-        elif rand == 3:
-            list += get_list()
-        if i != 3:
-            list += ','
-    list += ']'
-
-    dec_depth()
-    return list
-
-
-
-# get json object
-def get_obj():
+######All json objects#########
+def get_object():
     if depth_check() == 'FUCK':
         return '{}'
     inc_depth()
@@ -86,26 +57,53 @@ def get_obj():
     dec_depth()
     return obj
 
+def get_array():
+    if depth_check() == 'FUCK':
+        return ''
+    inc_depth()
 
-# return a list of tests
+    list = '['
+    for i in range(0,4):
+        rand = random.randint(0,4)
+        if rand == 0:
+            list += get_number()
+        elif rand == 1:
+            list += get_string()
+        elif rand == 2:
+            list += get_object()
+        elif rand == 3:
+            list += get_array()
+        if i != 3:
+            list += ','
+    list += ']'
+
+    dec_depth()
+    return list
+
+def get_value():
+    return random.choice([get_string, get_number, get_object, get_array, get_true, get_false, get_null])()
+
+def get_string():
+    fukd = ['\\','\"','\\\\','\/','\b','\f','\r','\t','\u','\u12fd','\uzxdg3'] # ,'\n'
+    temp = '\"'
+    temp += ''.join([random.choice(fukd) for _ in range(0, random.randint(0, 10))])
+    temp += get_chars()
+    temp += '\"'
+    return temp
+
+def get_number():
+    nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', 'e', 'E', '+', '-']
+    return ''.join([random.choice(nums) for _ in range(0, random.randint(0, 10))])
+
+##############################
+
 def get_tests():
     tests = []
     for x in range(1, get_int()):
-        obj = get_obj()
+        obj = get_object()
         tests.append(obj)
     return tests
 
-def get_true():
-    return random.choice(['true', 'True', 'TRUE', 't', 'T', '1'])
-
-def get_false():
-    return random.choice(['false', 'False', 'FALSE', 'f', 'F', '0'])
-
-def get_null():
-    return random.choice(['null', 'Null', 'NULL'])
-
-def get_value():
-    return random.choice([get_string, get_number, get_obj, get_list, get_true, get_false, get_null])()
 
 def main():
     global depth
